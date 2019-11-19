5c1646c5f9a60a65264db031a75adf46ca0f2cb9
Remy98 <57766336+Remy98@users.noreply.github.com>
2019-11-20 00:00:51 +0100

    Update Accueil.php

--- Accueil.php
+++ Accueil.php
@@ -5,9 +5,13 @@ To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
 <html>
+    <header>
+        <h1>LDLC</h1>
+        <!-- Ajouter une icone pour logo de la société-->
+    </header>
     <head>
         <meta charset="UTF-8">
-        <title> Acceuil </title>
+        <title> Accueil </title>
     </head>
     <body>
         <p> Bonjour. </p>
@@ -19,5 +23,10 @@ and open the template in the editor.
         <?php
         // put your code here
         ?>
+        <footer>
+            <h1>Nous contacter :</h1>
+            <p>Numéro de téléphone : 04 27 46 60 00</p><br>
+            <p>Adresse : 12-14 Rue de l'Église, 75015 Paris</p>
+        </footer>
     </body>
 </html>
