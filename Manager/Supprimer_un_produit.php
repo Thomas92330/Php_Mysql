d7cd88d678292716da3eb7617d78d1b45252ac9e
Thomas92330 <49101421+Thomas92330@users.noreply.github.com>
2019-11-19 19:44:45 +0100

    Update Supprimer_un_produit.php

--- Manager/Supprimer_un_produit.php
+++ Manager/Supprimer_un_produit.php
@@ -20,7 +20,31 @@ and open the template in the editor.
             </ul>
         </nav>
         <?php
-        // put your code here
+        require 'Database.php';    
+         $connection = new createConnexion();
+         $connect = $connection->connect();
+  
+  if($connect != NULL){
+      $req = mysqli_query("SELECT libelle FROM Produit");
+      while ($reponse = mysqli_fetch_array ($req))
+      {
+          $req2 = mysqli_prepare("SELECT quantite FROM Produit WHERE libelle = :reponse_libelle");
+          $req2 = mysqli_execute(array(
+              'reponse_libelle' => $reponse['libelle']
+          ));
+          while ($reponse_quantite = mysqli_fetch_array($req2)){
+                if($reponse_quantite['quantite'] == 0 ) {
+                    echo reponse['libelle'] . '<br/>';
+                }
+          }
+      }
+      
+      $resultat = mysqli_query($connect,$req);
+      if($resultat == false) {echo "Echec de la connexion <br>";}
+      else {echo "produit enregistré <br>";}
+      if(mysqli_close($connect)) {echo "Deconnexion reussi <br>";}
+  }
         ?>
+        
     </body>
 </html>
