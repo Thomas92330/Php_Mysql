512b2c4f7638ee0eb328473ca89fb4255a99fc94
Thomas92330 <49101421+Thomas92330@users.noreply.github.com>
2019-11-19 19:41:48 +0100

    Update Afficher_la_liste_des_commentaires_d_un_client_donné.php

--- Manager/Afficher_la_liste_des_commentaires_d_un_client_donné.php
+++ Manager/Afficher_la_liste_des_commentaires_d_un_client_donné.php
@@ -20,7 +20,22 @@ and open the template in the editor.
             </ul>
         </nav>
         <?php
-        // put your code here
+         require 'Database.php';    
+         $connection = new createConnexion();
+         $connect = $connection->connect();
+  
+  if($connect != NULL){
+      $req = mysqli_query("SELECT nom FROM Client");
+      while ($reponse = mysqli_fetch_array ($req))
+      {
+          echo $reponse['nom'] . '<br />';
+      }
+   
+      $resultat = mysqli_query($connect,$req);
+      if($resultat == false) {echo "Echec de la connexion <br>";}
+      else {echo "produit enregistré <br>";}
+      if(mysqli_close($connect)) {echo "Deconnexion reussi <br>";}
+  }
         ?>
     </body>
 </html>
