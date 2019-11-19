ca6ccc54819a4959f9efba70e49103f5a68167e8
Thomas92330 <49101421+Thomas92330@users.noreply.github.com>
2019-11-19 22:07:14 +0100

    Update Database.php

--- Database.php
+++ Database.php
@@ -22,7 +22,113 @@ class createConnexion  {
         mysqli_close($this->myconn);
         echo 'Connection closed!';
     }
-
+}
+        //Renvoi tout les id de la table client sous forme de tableau
+function get_id_client() {
+        
+}
+        //Renvoi tout les mdp de la table client sous forme de tableau
+function get_mdp_client() {
+    
+}   
+        //Renvoi tout les id de la table manager sous forme de tableau
+function get_id_manager() {
+    
+}
+        //Renvoi tout les mdp de la table manager sous forme de tableau
+function get_mdp_manager() {
+    
+}
+        //Renvoi tout les libelle de la table Produit sous forme de tableau
+function get_libelle(){
+    
+}
+        //Renvoi la quantite du produit 
+        //prend en argument libelle
+function get_quantite(){
+    
+}
+        //Ajoute un produit a la table Produit
+function ajoutez_un_produit() {
+    $req = mysqli_prepare("INSERT INTO Produit (Reference, Libelle, Categorie, Marque, Quantite, Prix, Description) VALUES (:reference,:libelle,:categorie,:marque,:quantite,:prix,:description)");
+    $req = mysqli_execute(array(
+          'reference' => $reference,
+          'libelle' => $libelle ,
+          'categorie' => $categorie,
+          'marque' => $marque,
+          'quantite' => $quantite,
+          'prix' => $prix,
+          'description' => $description
+      ));  
+}
+        //Supprime un produit de la table Produit 
+        //Prend en argument un libelle
+        //Verifier que la quantité du produit est a 0
+function supprimer_produit(){
+    $req = mysqli_query("SELECT libelle FROM Produit");
+      while ($reponse = mysqli_fetch_array ($req))
+      {
+          $req2 = mysqli_prepare("SELECT quantite FROM Produit WHERE libelle = :reponse_libelle");
+          $req2 = mysqli_execute(array(
+              'reponse_libelle' => $reponse['libelle']
+          ));
+          while ($reponse_quantite = mysqli_fetch_array($req2)){
+                if($reponse_quantite['quantite'] == 0 ) {
+                    echo reponse['libelle'] . '<br/>';
+                }
+          }
+      }
+      
+}
+        //Modifie les informations d'un élément produit
+        //Prend en Argument un libelle
+function modifier_produit() {
+    $req = mysqli_query("SELECT libelle FROM Produit");
+      while ($reponse = mysqli_fetch_array ($req))
+      {
+          echo $reponse['libelle'] . '<br />';
+      }
+   
+      $req = mysqli_prepare("UPDATE Produit SET Reference = :reference, Libelle = :libelle, Categorie = :categorie, Marque = :marque, Quantite = :quantite, Prix = :prix , Description = :description) WHERE libelle = :libelle_changer");
+      $req = mysqli_execute(array(
+          'libelle_changer' => $libelle_changer,
+          'reference' => $reference,
+          'libelle' => $libelle ,
+          'categorie' => $categorie,
+          'marque' => $marque,
+          'quantite' => $quantite,
+          'prix' => $prix,
+          'description' => $description
+      ));
+      
+}
+        //Affiche les commentaires d'un client
+        //Prend en argument un id de client
+function afficher_les_commentaires() {
+    $req = mysqli_query("SELECT nom FROM Client");
+      while ($reponse = mysqli_fetch_array ($req))
+      {
+          echo $reponse['nom'] . '<br />';
+      }
+   
+}
+        //Verifie l'authentification
+        //Prend en argument id et mdp d'un manager
+function authentification_manager() {
+    
+}
+        //Verifie l'authentification
+        //Prend en argument id et mdp d'un client
+function authentification_client() {
+    
+}
+        //Verifie la creation d'un compte
+        //Prend en argument id et mdp d'un cient
+        //Pas moins de 2 character
+        //Pas plus de 12
+        //Et bien plus encore, faut checker le sujet
+function verification_nouveau() {
+    
 }
 ?>
 
