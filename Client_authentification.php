d101813b43d9ddf6c6fd2df9f1ba7683b58455c7
Remy98 <57766336+Remy98@users.noreply.github.com>
2019-11-20 00:02:32 +0100

    Update Client_authentification.php

--- Client_authentification.php
+++ Client_authentification.php
@@ -38,5 +38,10 @@ and open the template in the editor.
             echo'<p>Mot de passe incorrecte</p>';
         }
         ?>
+         <footer>
+            <h1>Nous contacter :</h1>
+            <p>Numéro de téléphone : 04 27 46 60 00</p><br>
+            <p>Adresse : 12-14 Rue de l'Église, 75015 Paris</p>
+        </footer>
     </body>
 </html>
