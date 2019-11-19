01f488e3fcf206361c4adfad67b4dd2cd2f82cea
Remy98 <57766336+Remy98@users.noreply.github.com>
2019-11-20 00:03:13 +0100

    Update Manager_authentification.php

--- Manager_authentification.php
+++ Manager_authentification.php
@@ -36,5 +36,10 @@ and open the template in the editor.
             echo $_POST['manager_mot_de_passe'];
         }
         ?>
+         <footer>
+            <h1>Nous contacter :</h1>
+            <p>Numéro de téléphone : 04 27 46 60 00</p><br>
+            <p>Adresse : 12-14 Rue de l'Église, 75015 Paris</p>
+        </footer>
     </body>
 </html>
