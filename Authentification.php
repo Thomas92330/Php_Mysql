d48e0816fd73dc26bb8344cd02d3d1824c086854
Thomas92330 <49101421+Thomas92330@users.noreply.github.com>
2019-11-21 22:46:26 +0100

    Update Authentification.php

--- Authentification.php
+++ Authentification.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<<!DOCTYPE html>
 <!--
 To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
@@ -10,11 +10,15 @@ and open the template in the editor.
         <title>Authentification</title>
     </head>
     <body>
-
+      
+    
+        
         <?php
         include 'Database.php';
         
-        if ((isset($_POST['client'])== FALSE) && (isset($_POST['manager'])== FALSE) && (isset($_POST['nouveau'])== FALSE)){
+        if ((isset($_POST['client'])== FALSE) && (isset($_POST['manager'])== FALSE) 
+         && (isset($_POST['client_id'])== FALSE) && (isset($_POST['client_mot_de_passe'])== FALSE ) 
+         && (isset($_POST['manager_id'])== FALSE) && (isset($_POST['manager_mot_de_passe'])== FALSE )){
             echo'
                 <form action="Authentification.php" method="post">
             <p>
@@ -43,21 +47,18 @@ and open the template in the editor.
         }
         
         if (isset($_POST['client_id']) && isset($_POST['client_mot_de_passe'])){
-            require 'Database.php';    
-            $connection = new createConnexion();
-            $connect = $connection->connect();
-  
-            if($connect != NULL){
-                $req = mysqli_query("SELECT id FROM Client");
-                while ($reponse = mysqli_fetch_array ($req))
-                {
-                    echo $reponse['libelle'] . '<br />';
-                }
-
-                $resultat = mysqli_query($connect,$req);
-                if($resultat == false) {echo "Echec de la connexion <br>";}
-                else {echo "produit enregistré <br>";}
-                if(mysqli_close($connect)) {echo "Deconnexion reussi <br>";}
+            if(authentification($_POST['client_id'], $_POST['client_mot_de_passe'])){
+                echo'
+            <nav class="menu">
+            <ul>
+                <li><a href="Inscription.php"> Ajouter un produit </a></li>
+                <li><a href="Modifier_ses_informations.php"> Modifier un produit </a>
+                <li><a href="Consulter_son_profil.php"> Consulter un produit </a></li>    
+                <li><a href="Passer_une_commande.php"> Passer une commande</a></li>
+                <li><a href="Ajouter_un_commentaire.php"> Ajouter un commentaire </a></li>
+            </ul>
+            </nav>  
+        ';
             }
         }
         
@@ -75,9 +76,20 @@ and open the template in the editor.
         }
         
         if (isset($_POST['manager_id']) && isset($_POST['manager_mot_de_passe'])){
-
-        }
-        
+            if(authentification($_POST['manager_id'], $_POST['manager_mot_de_passe'])){
+                echo '
+            <nav class="menu">
+            <ul>
+                <li><a href="Ajoutez_un_produit.php"> Ajouter un produit </a>
+                <li><a href="Modifier_un_produit.php"> Modifier un produit </a></li>
+                <li><a href="Supprimer_un_produit.php"> Supprimer un produit </a></li>
+                <li><a href="Consulter_un_produit.php"> Consulter un produit </a></li>
+                <li><a href="Afficher_la_liste_des_commentaires_d_un_client_donné.php"> Afficher la liste des commentaires d un client donné</a></li>
+            </ul>
+            </nav> 
+            ';
+            }
+        }    
 
         ?>
          <footer>
