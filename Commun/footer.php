fa79db8a7792b794c351d8677c753f00a55596ce
Thomas92330 <49101421+Thomas92330@users.noreply.github.com>
2019-11-22 15:44:49 +0100

    Add files via upload

---
+++ Commun/footer.php
@@ -0,0 +1,5 @@
+<footer>
+        <h1>Nous contacter :</h1>
+        <p>Numéro de téléphone : 04 27 46 60 00</p><br>
+        <p>Adresse : 12-14 Rue de l'Église, 75015 Paris</p>
+</footer>
---
+++ Commun/nav_client.php
@@ -0,0 +1,6 @@
+<nav >
+                <a href="../Client/Modifier_ses_informations.php"> Modifier ses information   </a>;
+                <a href="../Client/Consulter_son_profil.php"> Consulter son profil </a>    
+                <a href="../Client/Passer_une_commande.php"> Passer une commande</a>
+                <a href="../Client/Ajouter_un_commentaire.php"> Ajouter un commentaire </a>
+</nav>
