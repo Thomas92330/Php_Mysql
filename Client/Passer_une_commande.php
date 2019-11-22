505ba917cb21eca93452dff2bce4ea32461ef7ca
Thomas92330 <49101421+Thomas92330@users.noreply.github.com>
2019-11-22 15:51:46 +0100

    Update Passer_une_commande.php

--- Client/Passer_une_commande.php
+++ Client/Passer_une_commande.php
@@ -10,15 +10,8 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
-        <nav class="menu">
-            <ul>
-                <li><a href="Inscription.php"> Ajouter un produit </a></li>
-                <li><a href="Modifier_ses_informations.php"> Modifier un produit </a>
-                <li><a href="Consulter_son_profil.php"> Consulter un produit </a></li>    
-                <li><a href="Passer_une_commande.php"> Passer une commande</a></li>
-                <li><a href="Ajouter_un_commentaire.php"> Ajouter un commentaire </a></li>
-            </ul>
-        </nav>
+        <?php include '../Commun/nav_client.php'; ?>
+        
         <?php
         // put your code here
         ?>
