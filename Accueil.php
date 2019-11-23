af655b4163bfc0807f10c1224f88d3bc2ab877e3
Remy98 <57766336+Remy98@users.noreply.github.com>
2019-11-23 13:34:44 +0100

    Update Accueil.php

--- Accueil.php
+++ Accueil.php
@@ -5,24 +5,34 @@ To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
 <html>
-    <header>
-        <h1>LDLC</h1>
-        <!-- Ajouter une icone pour logo de la société-->
-    </header>
     <head>
+        <link rel="stylesheet" href="accueil.css">
         <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title> Accueil </title>
+        <!-- Ajouter une icone pour logo de la société-->
     </head>
     <body>
-        <p> Bonjour. </p>
-            
-        <p> Souhaitez-vous vous <a href="Authentification.php"> connecter </a> ?</p>
-        <p> Ou alors <a href="Creation_de_compte.php"> creer un compte </a> ?</p>
+        <header>
+            <figure>
+                <img id="logo_ldlc" src="ldlc-logo.jpg" alt="logo" width="180" height="180"/>
+            </figure>
+            <h1>LDLC</h1>
+       </header>
+        <img id="connexion_logo" src="connexion_logo.jpg" alt="flottante" width="150" height="60"/>
+        <p id="connexion"><br> Bonjour, souhaitez-vous vous <a href="Authentification.php"> connecter </a> ?</p>
+        <p id="connexion2"> Ou alors <a href="Creation_de_compte.php"> creer un compte </a> ?</p>
         
-        <p>Créé en 1996, le Groupe LDLC est aujourd’hui leader du e-commerce informatique et high-tech en France. Avec ses 5 sites marchands, il conjugue des<br/>
-            activités dans le domaine de l’informatique, du high-tech, de la puériculture ou encore de l’éducation. En avril 2019, le Groupe LDLC emploie environ 920<br/> 
+        <img id="info" src="info_logo.png" alt="info" width="60" height="60"/>
+        <article>
+        <p>Créé en 1996, le Groupe LDLC est aujourd’hui leader du e-commerce informatique et high-tech en France. Avec ses 5 sites marchands, il conjugue des
+            activités dans le domaine de l’informatique, du high-tech, de la puériculture ou encore de l’éducation. En avril 2019, le Groupe LDLC emploie environ 920
             collaborateurs pour un chiffre d’affaires de 507,5 millions d’euros.</p>
-        
-        <?php include 'Commun/footer.php'; ?>
+
+        </article>
+        <footer>
+            <h2>Nous contacter :</h2>
+            <p>Numéro de téléphone : 04 27 46 60 00 // Adresse : 12-14 Rue de l'Église, 75015 Paris</p>
+        </footer>
     </body>
 </html>
