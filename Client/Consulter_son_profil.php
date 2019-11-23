3f595b1493479d5131be625beb98e52250c43b5f
Remy98 <57766336+Remy98@users.noreply.github.com>
2019-11-23 22:29:13 +0100

    Update Consulter_son_profil.php

--- Client/Consulter_son_profil.php
+++ Client/Consulter_son_profil.php
@@ -6,13 +6,23 @@ and open the template in the editor.
 -->
 <html>
     <head>
+        <link rel="stylesheet" href="Authentification.css">
         <meta charset="UTF-8">
         <title></title>
     </head>
     <body>
+        <header>
+            <figure>
+                <img id="logo_ldlc" src="ldlc-logo.jpg" alt="logo" width="180" height="180"/>
+            </figure>
+            <h1>LDLC</h1>
+       </header>
+        <div>
         <?php include '../Commun/nav_client.php'; ?>
         <?php
         // put your code here
         ?>
+        </div>
+        <?php include 'Commun/footer.php'; ?>
     </body>
 </html>
