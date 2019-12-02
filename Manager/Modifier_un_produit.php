a9fa33e508f3b0ef3581f6d09ce9fb49188eae94
Thomas92330 <49101421+Thomas92330@users.noreply.github.com>
2019-12-02 14:42:36 +0100

    Update Modifier_un_produit.php

--- Manager/Modifier_un_produit.php
+++ Manager/Modifier_un_produit.php
@@ -6,52 +6,93 @@ and open the template in the editor.
 -->
 <html>
     <head>
-        <link rel="stylesheet" href="Authentification.css">
+        <link rel="stylesheet" href="../Communn/authentification.css">
         <meta charset="UTF-8">
         <title></title>
     </head>
     <body>
-        <header>
-            <figure>
-                <img id="logo_ldlc" src="ldlc-logo.jpg" alt="logo" width="180" height="180"/>
-            </figure>
-            <h1>LDLC</h1>
-       </header>
-        <div>
-        <?php include '../Commun/nav_manager.php'; ?>
-        <?php       
-        if(isset($_POST['checker'])){
-        include 'Database.php';    
-        $connection = new createConnexion();
-        $connect = $connection->connect();
+        <?php include '../Commun/nav.php'; 
+              include '../Commun/Database.php';    ?>
+        
+                  <table>
+            <th>
+            <tr>'Libelle'</tr>
+            <tr>'Categorie'</tr>
+            <tr>'Marque'</tr>
+            <tr>'Description'</tr>
+            <tr>'Prix'</tr>
+        </th>
 
-        if($connect != NULL){
-             modifier_un_produit($_POST['libelle'], $_POST['categorie'], $_POST['$marque'], $_POST['quantite'], $_POST['prix'], $_POST['description']);
-        }
-            
-        }
-         
-        else{
-            echo'
-                 <form action="Modifier_un_produit.php" method="post">
-            <p>
-                <input type="text" name="libelle">
-                <input type="text" name="categorie">
-                <input type="text" name="marque">
-                <input type="number" name="quantite">
-                <input type="number" name="prix">
-                <input type="text" name="description">
-                
-                <input type="hidden" name="checker" value=1>
-                
+        <th>
+        <tr>
+        <form action="Modifier_un_produit.php" method="get">'
+            <input type="radio" id="btn" name="choix" value ="Libelle"/> <label for="btn">Modifier</label>
+            <input type="text" id="btn" name="valeure" /> <label for="btn">Nouvelle 
+                <input type="hidden" name="reference" value=<?php$_GET['reference']?>/>
+            <input type="submit" value="Valider">
+            </tr>
+            <tr>
+            <form action="Modifier_un_produit.php" method="get">'
+                <input type="radio" id="btn" name="choix" value ="Categorie"/> <label for="btn">Modifier</label>
+                <input type="text" id="btn" name="valeure" /> <label for="btn">Nouvelle 
+                    <input type="hidden" name="reference" value=<?php$_GET['reference']?>/>
                 <input type="submit" value="Valider">
-            </p>
-            ';
-             
-        }
+                </tr>
+                <tr>
+                <form action="Modifier_un_produit.php" method="get">'
+                    <input type="radio" id="btn" name="choix" value ="Marque"/> <label for="btn">Modifier</label>
+                    <input type="text" id="btn" name="valeure" /> <label for="btn">Nouvelle 
+                        <input type="hidden" name="reference" value=<?php$_GET['reference']?>/>
+                    <input type="submit" value="Valider">
+                    </tr>
+
+                    <tr>
+                    <form action="Modifier_un_produit.php" method="get">'
+                        <input type="radio" id="btn" name="choix" value ="Description"/> <label for="btn">Modifier</label>
+                        <input type="telephone" id="btn" name="valeure" /> <label for="btn">Nouvelle 
+                            <input type="hidden" name="reference" value=<?php$_GET['reference']?>/>
+                        <input type="submit" value="Valider">
+                        </tr>
+
+                        <tr>
+                        <form action="Modifier_un_produit.php" method="get">'
+                            <input type="radio" id="btn" name="choix" value ="Prix"/> <label for="btn">Modifier</label>
+                            <input type="number" id="btn" name="valeure" /> <label for="btn">Nouvelle 
+                                <input type="hidden" name="reference" value=<?php$_GET['reference']?>/>
+                            <input type="submit" value="Valider">
+                            </tr>
+                            <tr>
+                                        </th>
+
+
+                                        <?php
+                                        if (isset($_GET['choix'])) {
+                                            if ($_GET['choix'] == 'Libelle' && isset($_GET['valeure'])) {
+                                                modifier_produit($_GET['valeure'], 'Libelle',$_GET['reference']);
+                                            }
+
+                                            if ($_GET['choix'] == 'Categorie' && isset($_GET['valeure'])) {
+                                                modifier_produit($_GET['valeure'], 'Categorie',$_GET['reference']);
+                                            }
+
+                                            if ($_GET['choix'] == 'Marque' && isset($_GET['valeure'])) {
+                                                modifier_produit($_GET['valeure'], 'Marque',$_GET['reference']);
+                                            }
+
+                                            if ($_GET['choix'] == 'Prix' && isset($_GET['valeure'])) {
+                                                modifier_produit($_GET['valeure'], 'Prix',$_GET['reference']);
+                                            }
+
+                                            if ($_GET['choix'] == 'Description' && isset($_GET['valeure'])) {
+                                                modifier_produit($_GET['valeure'], 'Description',$_GET['reference']);
+                                            }
+                                        }
+                                        ?>
+                                        </body>
+                                        </html>
+            
+       
         
         ?>
-        </div>
-        <?php include 'Commun/footer.php'; ?>
     </body>
 </html>
