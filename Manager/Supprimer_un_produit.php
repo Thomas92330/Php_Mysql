399bdc6516af43b44245d4998d22daacfbbb7447
Thomas92330 <49101421+Thomas92330@users.noreply.github.com>
2019-12-02 14:43:19 +0100

    Update Supprimer_un_produit.php

--- Manager/Supprimer_un_produit.php
+++ Manager/Supprimer_un_produit.php
@@ -6,45 +6,49 @@ and open the template in the editor.
 -->
 <html>
     <head>
-        <link rel="stylesheet" href="Authentification.css">
+        <link rel="stylesheet" href="../Communn/authentification.css">
         <meta charset="UTF-8">
         <title></title>
     </head>
     <body>
-        <header>
-            <figure>
-                <img id="logo_ldlc" src="ldlc-logo.jpg" alt="logo" width="180" height="180"/>
-            </figure>
-            <h1>LDLC</h1>
-       </header>
-        <div>
-        <?php include '../Commun/nav_manager.php'; ?>
-        <?php
-        if(isset($_POST['checker'])){
-        include 'Database.php';    
-        $connection = new createConnexion();
-        $connect = $connection->connect();
-
-        if($connect != NULL){
-            supprimer_produit($libelle);
-        }
-            
-        }
-         
-        else{
-            echo'
-                 <form action="Supprimer_un_produit.php" method="post">
-            <p>
-                <input type="text" name="libelle">
-                <input type="hidden" name="checker" value=1>
-                
-                <input type="submit" value="Valider">
-            </p>
-            ';
-             
-        }
-        ?>
-        </div>
-        <?php include 'Commun/footer.php'; ?>
+        <?php include '../Commun/nav_manager.php'; 
+              include '../Commun/Database.php'?>
+        <table>
+    <tr>
+       <th>'Référence.'</th>
+       <th>'Libellé.'</th>
+       <th>'Catégorie.'</th>
+       <th>'Marque.'</th>
+       <th>'Quantité.'</th>
+       <th>'Prix.'</th>
+       <th>'Description.'</th>
+       <th>'Supprimer.'</th>
+    </tr>
+    
+    <?php   
+    $connection = new createConnexion();
+    $connect = $connection->connect();
+    $result = mysqli_query($connect,"SELECt * FROM Produit");
+    while ($res = mysqli_fetch_assoc($result))
+    {
+        if($res['Quantite']==0){
+    echo'<tr>';
+       echo'<th>'.$res['Reference'].'</th>';
+       echo'<th>'.$res['Libelle'].'</th>';
+       echo'<th>'.$res['Categorie'].'</th>';
+       echo'<th>'.$res['Marque'].'</th>';
+       echo'<th>'.$res['Quantite'].'</th>';
+       echo'<th>'.$res['Prix'].'</th>';
+       echo'<th>'.$res['Description'].'</th>';
+       echo'<th>'
+       . '<form action="Supprimer_un_produit.php" method="get">'
+       . '<input type="radio" id="btn" name="produit" value ='.$res["Reference"].'/> <label for="btn">Supprimer</label>      
+        <input type="submit" value="Valider">'
+       . '</th>';
+    echo'</tr>';
+    } 
+    }
+?>
+        
     </body>
 </html>
