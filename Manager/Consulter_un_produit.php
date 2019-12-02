d4fef0910f132f3d68372e100abd68582b525da0
Thomas92330 <49101421+Thomas92330@users.noreply.github.com>
2019-12-02 14:41:54 +0100

    Update Consulter_un_produit.php

--- Manager/Consulter_un_produit.php
+++ Manager/Consulter_un_produit.php
@@ -6,29 +6,48 @@ and open the template in the editor.
 -->
 <html>
     <head>
-        <link rel="stylesheet" href="Authentification.css">
+        <link rel="stylesheet" href="../Communn/authentification.css">
         <meta charset="UTF-8">
         <title></title>
     </head>
     <body>
-        <header>
-            <figure>
-                <img id="logo_ldlc" src="ldlc-logo.jpg" alt="logo" width="180" height="180"/>
-            </figure>
-            <h1>LDLC</h1>
-       </header>
-        <div>
-        <?php include '../Commun/nav_manager.php'; ?>
-        <?php
-         require 'Database.php';    
-         $connection = new createConnexion();
-         $connect = $connection->connect();
-  
-         if($connect != NULL){
-      
-  }
+        <?php include '../Commun/nav.php'; 
+              include '../Commun/Database.php'?>
+        <table>
+    <tr>
+       <th>'Référence.'</th>
+       <th>'Libellé.'</th>
+       <th>'Catégorie.'</th>
+       <th>'Marque.'</th>
+       <th>'Quantité.'</th>
+       <th>'Prix.'</th>
+       <th>'Description.'</th>
+    </tr>
+    
+    <?php   
+    $connection = new createConnexion();
+    $connect = $connection->connect();
+    $result = mysqli_query($connect,"SELECT * FROM Produit");
+    while ($res = mysqli_fetch_assoc($result))
+    {
+        if($res['Quantite']==0){
+    echo'<tr>';
+       echo'<th>'.$res['Reference'].'</th>';
+       echo'<th>'.$res['Libelle'].'</th>';
+       echo'<th>'.$res['Categorie'].'</th>';
+       echo'<th>'.$res['Marque'].'</th>';
+       echo'<th>'.$res['Quantite'].'</th>';
+       echo'<th>'.$res['Prix'].'</th>';
+       echo'<th>'.$res['Description'].'</th>';
+       echo'<th>'
+                    . '<form action="Modifier_un_produit.php" method="get">'
+                    . '<input type="radio" id="btn" name="reference" value =' . $res["Reference"] . '/> <label for="btn">Modifier</label>      
+        <input type="submit" value="Valider">'
+                    . '</form>'
+                    . '</th>';
+                    
+    } 
+    }
         ?>
-        </div>
-        <?php include 'Commun/footer.php'; ?>"
     </body>
 </html>
