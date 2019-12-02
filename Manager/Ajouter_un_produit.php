142ee9b6c1a60d0f951638100c202a30a4a78da3
Thomas92330 <49101421+Thomas92330@users.noreply.github.com>
2019-12-02 14:41:02 +0100

    Update Ajouter_un_produit.php

--- Manager/Ajouter_un_produit.php
+++ Manager/Ajouter_un_produit.php
@@ -6,33 +6,23 @@ and open the template in the editor.
 -->
 <html>
     <head>
-        <link rel="stylesheet" href="Authentification.css">
+        <link rel="stylesheet" href="../Commun/authentification.css">
         <meta charset="UTF-8">
         <title></title>
     </head>
     <body>
-        <header>
-            <figure>
-                <img id="logo_ldlc" src="ldlc-logo.jpg" alt="logo" width="180" height="180"/>
-            </figure>
-            <h1>LDLC</h1>
-       </header>
-        <div>
         <?php include '../Commun/nav_manager.php'; ?>
         <?php
-         if(isset($_POST['checker'])){
-         include 'Database.php';    
-         $connection = new createConnexion();
-         $connect = $connection->connect();
-  
-         if($connect != NULL){
-             ajoutez_un_produit($_POST['libelle'], $_POST['categorie'], $_POST['$marque'], $_POST['quantite'], $_POST['prix'], $_POST['description']);
+        if (isset($_POST['checker'])) {
+            include 'Database.php';
+            $connection = new createConnexion();
+            $connect = $connection->connect();
+
+            if ($connect != NULL) {
+                ajoutez_un_produit($_POST['libelle'], $_POST['categorie'], $_POST['$marque'], $_POST['quantite'], $_POST['prix'], $_POST['description']);
             }
-            
-            }
-         
-         else{
-             echo'
+        } else {
+            echo'
                  <form action="Ajouter_un_produit.php" method="post">
             <p>
                 <input type="text" name="libelle">
@@ -47,11 +37,7 @@ and open the template in the editor.
                 <input type="submit" value="Valider">
             </p>
             ';
-             
-         }
-
+        }
         ?>
-        </div>
-        <?php include 'Commun/footer.php'; ?>
     </body>
 </html>
