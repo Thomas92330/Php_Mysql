24129270ffc1086b2c9aa8d701597461da3ecb59
Thomas92330 <49101421+Thomas92330@users.noreply.github.com>
2019-12-02 14:40:16 +0100

    Update Afficher_la_liste_des_commentaires_d_un_client_donné.php

--- Manager/Afficher_la_liste_des_commentaires_d_un_client_donné.php
+++ Manager/Afficher_la_liste_des_commentaires_d_un_client_donné.php
@@ -1,34 +1,64 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
     <head>
-        <link rel="stylesheet" href="Authentification.css">
+        <link rel="stylesheet" href="../Css/Authentification.css">
         <meta charset="UTF-8">
         <title></title>
     </head>
     <body>
+
         <header>
             <figure>
-                <img id="logo_ldlc" src="ldlc-logo.jpg" alt="logo" width="180" height="180"/>
+                <img id="logo_ldlc" src="../Css/ldlc-logo.jpg" alt="logo" width="180" height="180"/>
             </figure>
             <h1>LDLC</h1>
-       </header>
-        <div>
-        <?php include '../Commun/nav_manager.php'; ?>
-        <?php
-         include 'Database.php';    
-         $connection = new createConnexion();
-         $connect = $connection->connect();
-  
-        if($connect != NULL){
-      
-  }
-        ?>
+        </header>
+        
+            <?php
+            restore_include_path();
+            include '../Commun/Database.php';
+            restore_include_path();
+            include '../Commun/nav.php';
+            ?>
+            
+            <?php if(!isset($_GET['client'])){
+            echo'<table>';
+                echo'<tr>';
+                    echo'<th>Nom</th>';
+                    echo'<th>Prenom</th>';
+                    echo'<th>Id</th>';
+                    echo'<th>Consulter</th>';
+                echo'</tr>';
+
+                
+                $connection = new createConnexion();
+                $connect = $connection->connect();
+                $result = mysqli_query($connect, "SELECT * FROM Utilisateur WHERE Client = 1");
+                while ($res = mysqli_fetch_assoc($result)) {
+                    echo'<tr>';
+                    echo'<th>' . $res['Nom'] . '</th>';
+                    echo'<th>' . $res['Prenom'] . '</th>';
+                    echo'<th>' . $res['Id'] . '</th>';
+   
+                    echo'<th>'
+                    . '<form action="Afficher_la_liste_des_commentaires_d_un_client_donné.php" method="get">'
+                    . '<input type="radio" id="btn" name="client" value =' . $res["Id"] . '/> <label for="btn">Consulter</label>      
+        <input type="submit" value="Valider">'
+                    . '</form>'.'</th>';
+        
+                    echo'</tr>';
+                }
+            }
+            
+            else {
+                afficher_les_commentaires($_GET['client']);
+            }
+                ?>
+            </table>
         </div>
-        <?php include 'Commun/footer.php'; ?>
+        <?php
+        restore_include_path();
+        include '../Commun/footer.php';
+        ?>
     </body>
+
 </html>
