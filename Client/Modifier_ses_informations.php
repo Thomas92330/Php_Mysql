074fb0a8b7443d3f0bc56d2cdf8bfa6d4cc48567
Thomas92330 <49101421+Thomas92330@users.noreply.github.com>
2019-12-10 22:50:24 +0100

    Update Modifier_ses_informations.php

--- Client/Modifier_ses_informations.php
+++ Client/Modifier_ses_informations.php
@@ -11,108 +11,20 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
-        <?php include '../Commun/nav.php';
-        include '../Commun/Database.php';
+        <?php 
+        include_once '/opt/lampp/htdocs/ProjetPhp/Commun/nav.php';
         ?>
         <table>
-            <th>
-            <tr>'Nom'</tr>
-            <tr>'Prénom'</tr>
-            <tr>'Age'</tr>
-            <tr>'Telephone'</tr>
-            <tr>'Adresse'</tr>
-            <tr>'Genre'</tr>
-            <tr>'Date de naissance'</tr>
-            <tr>'Situation familiale'</tr>
-        </th>
-
-        <th>
-        <tr>
-        <form action="Modifier_ses_informations.php" method="get">'
-            <input type="radio" id="btn" name="choix" value ="Nom"/> <label for="btn">Modifier</label>
-            <input type="text" id="btn" name="valeure" /> <label for="btn">Nouvelle valeure</label>
-            <input type="submit" value="Valider">
-            </tr>
-            <tr>
-            <form action="Modifier_ses_informations.php" method="get">'
-                <input type="radio" id="btn" name="choix" value ="Prenom"/> <label for="btn">Modifier</label>
-                <input type="text" id="btn" name="valeure" /> <label for="btn">Nouvelle valeure</label>
-                <input type="submit" value="Valider">
-                </tr>
-                <tr>
-                <form action="Modifier_ses_informations.php" method="get">'
-                    <input type="radio" id="btn" name="choix" value ="Age"/> <label for="btn">Modifier</label>
-                    <input type="text" id="btn" name="valeure" /> <label for="btn">Nouvelle valeure</label>
-                    <input type="submit" value="Valider">
-                    </tr>
-
-                    <tr>
-                    <form action="Modifier_ses_informations.php" method="get">'
-                        <input type="radio" id="btn" name="choix" value ="Telephone"/> <label for="btn">Modifier</label>
-                        <input type="number" id="btn" name="valeure" /> <label for="btn">Nouvelle valeure</label>
-                        <input type="submit" value="Valider">
-                        </tr>
-
-                        <tr>
-                        <form action="Modifier_ses_informations.php" method="get">'
-                            <input type="radio" id="btn" name="choix" value ="Adresse"/> <label for="btn">Modifier</label>
-                            <input type="text" id="btn" name="valeure" /> <label for="btn">Nouvelle valeure</label>
-                            <input type="submit" value="Valider">
-                            </tr>
-
-                            <tr>
-                            <form action="Modifier_ses_informations.php" method="get">'
-                                <input type="radio" id="btn" name="choix" value ="Sexe"/> <label for="btn">Modifier</label>
-                                <input type="text" id="btn" name="valeure" /> <label for="btn">Nouvelle valeure</label>
-                                <input type="submit" value="Valider">
-                                </tr>
-
-                                <tr>
-                                <form action="Modifier_ses_informations.php" method="get">'
-                                    <input type="radio" id="btn" name="choix" value ="Date_de_naissance"/> <label for="btn">Modifier</label>
-                                    <input type="date" id="btn" name="valeure" /> <label for="btn">Nouvelle valeure</label>
-                                    <input type="submit" value="Valider">
-                                    </tr>
-
-                                    <tr>
-                                    <form action="Modifier_ses_informations.php" method="get">'
-                                        <input type="radio" id="btn" name="choix" value ="Familiale"/> <label for="btn">Modifier</label>
-                                        <input type="text" id="btn" name="valeure" /> <label for="btn">Nouvelle valeure</label>
-                                        <input type="submit" value="Valider">
-                                        </tr>
-                                        </th>
-
-
-                                        <?php
-                                        if (isset($_GET['choix'])) {
-                                            if ($_GET['choix'] == 'Nom' && isset($_GET['valeure'])) {
-                                                modifier_info($_GET['valeure'], 'Nom');
-                                            }
-
-                                            if ($_GET['choix'] == 'Prenom' && isset($_GET['valeure'])) {
-                                                modifier_info($_GET['valeure'], 'Prenom');
-                                            }
-
-                                            if ($_GET['choix'] == 'Age' && isset($_GET['valeure'])) {
-                                                modifier_info($_GET['valeure'], 'Age');
-                                            }
-
-                                            if ($_GET['choix'] == 'Adresse' && isset($_GET['valeure'])) {
-                                                modifier_info($_GET['valeure'], 'Adresse');
-                                            }
-
-                                            if ($_GET['choix'] == 'Telephone' && isset($_GET['valeure'])) {
-                                                modifier_info($_GET['valeure'], 'Telephone');
-                                            }
-
-                                            if ($_GET['choix'] == 'Sexe' && isset($_GET['valeure'])) {
-                                                modifier_info($_GET['valeure'], 'Sexe');
-                                            }
-
-                                            if ($_GET['choix'] == 'Familiale' && isset($_GET['valeure'])) {
-                                                modifier_info($_GET['valeure'], 'Familiale');
-                                            }
-                                        }
-                                        ?>
-                                        </body>
-                                        </html>
+        <?php 
+        if(!isset($_GET['choix'])){
+            modifier_info_formulaire();
+        }
+        else{
+            modifier_info_util(); 
+        }
+        ?>
+        </table>
+    </body>
+</html>
+        
+      
