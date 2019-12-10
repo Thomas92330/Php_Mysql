9e07de361e4a1d073e0a732d3002a1398d25d364
Thomas92330 <49101421+Thomas92330@users.noreply.github.com>
2019-12-10 22:48:10 +0100

    Update Achete.php

--- Client/Achete.php
+++ Client/Achete.php
@@ -13,80 +13,17 @@
         </header>
         <div>
             <?php
-            restore_include_path();
-            include '../Commun/Database.php';
-            restore_include_path();
-            include '../Commun/nav.php';
+            include_once '/opt/lampp/htdocs/ProjetPhp/Commun/Database.php';
+            include_once '/opt/lampp/htdocs/ProjetPhp/Commun/nav.php';
             ?>
 
 
             <?php
             if (isset($_GET['produit']) && !isset($_GET['quantite'])) {
-                $connection = new createConnexion();
-                $connect = $connection->connect();
-
-
-                $result = mysqli_prepare($connect, "SELECT * FROM Produit WHERE Reference = ?");
-                $result->bind_param('s', $_GET['produit']);
-                $result->execute();
-                $result->bind_result($reference, $libelle, $categorie, $marque, $quantite, $prix, $description);
-                $result->fetch();
-
-
-                echo'<table>
-    <tr>
-       <th>Référence</th>
-       <th>Libellé</th>
-       <th>Catégorie</th>
-       <th>Marque</th>
-       <th>Quantité disponible</th>
-       <th>Prix</th>
-       <th>Quantité souhaité</th>
-    </tr>
-    
-    <tr>
-       <th>' . $reference . '</th>
-       <th>' . $libelle . '</th>
-       <th>' . $categorie . '</th>
-       <th>' . $marque . '</th>
-       <th>' . $quantite . '</th>
-       <th>' . $prix . '</th>
-       <th>
-          <form action="Achete.php" method="get">
-          <input type="number" id="btn" name="quantite" />  
-          <input type="hidden" name="produit" value=' . $reference . '
-        <input type="submit" value="Valider">
-       </th>
-       <th>
-       
-    </tr>';
+                tableau_quantite_achete();
             }
-
             if (isset($_GET['produit']) && isset($_GET['quantite'])) {
-
-                $connection = new createConnexion();
-                $connect = $connection->connect();
-
-                $result = mysqli_prepare($connect, "SELECT * FROM Produit WHERE Reference = ?");
-                $result->bind_param('s', $_GET['produit']);
-                $result->execute();
-                $result->bind_result($reference, $libelle, $categorie, $marque, $quantite, $prix, $description);
-                $result->fetch();
-                $result->close();
-
-                $reste = $quantite - $_GET['quantite'];
-                if ($reste > 0) {
-                    if ($result = mysqli_prepare($connect, "UPDATE Produit SET Quantite = ? WHERE Reference = ?")) {
-                        $result->bind_param('ss', $reste, $_GET['produit']);
-                        if ($result->execute()) {
-                            echo'oui!';
-                        }
-                    } else {
-                        echo 'non!!';
-                    }
-                } else {
-                    echo 'Quantite demande trop grande';
-                }
+                achete();
             }
             ?>
         </div>                
