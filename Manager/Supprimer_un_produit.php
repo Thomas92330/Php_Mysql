92562b0c0bfaa5889436850bc19f5d66994bdb0f
Thomas92330 <49101421+Thomas92330@users.noreply.github.com>
2019-12-10 22:55:13 +0100

    Update Supprimer_un_produit.php

--- Manager/Supprimer_un_produit.php
+++ Manager/Supprimer_un_produit.php
@@ -6,49 +6,23 @@ and open the template in the editor.
 -->
 <html>
     <head>
-        <link rel="stylesheet" href="../Communn/authentification.css">
+        <link rel="stylesheet" href="../Commun/authentification.css">
         <meta charset="UTF-8">
         <title></title>
     </head>
     <body>
-        <?php include '../Commun/nav_manager.php'; 
-              include '../Commun/Database.php'?>
-        <table>
-    <tr>
-       <th>'Référence.'</th>
-       <th>'Libellé.'</th>
-       <th>'Catégorie.'</th>
-       <th>'Marque.'</th>
-       <th>'Quantité.'</th>
-       <th>'Prix.'</th>
-       <th>'Description.'</th>
-       <th>'Supprimer.'</th>
-    </tr>
-    
-    <?php   
-    $connection = new createConnexion();
-    $connect = $connection->connect();
-    $result = mysqli_query($connect,"SELECt * FROM Produit");
-    while ($res = mysqli_fetch_assoc($result))
-    {
-        if($res['Quantite']==0){
-    echo'<tr>';
-       echo'<th>'.$res['Reference'].'</th>';
-       echo'<th>'.$res['Libelle'].'</th>';
-       echo'<th>'.$res['Categorie'].'</th>';
-       echo'<th>'.$res['Marque'].'</th>';
-       echo'<th>'.$res['Quantite'].'</th>';
-       echo'<th>'.$res['Prix'].'</th>';
-       echo'<th>'.$res['Description'].'</th>';
-       echo'<th>'
-       . '<form action="Supprimer_un_produit.php" method="get">'
-       . '<input type="radio" id="btn" name="produit" value ='.$res["Reference"].'/> <label for="btn">Supprimer</label>      
-        <input type="submit" value="Valider">'
-       . '</th>';
-    echo'</tr>';
-    } 
+        <?php
+        include_once '/opt/lampp/htdocs/ProjetPhp/Commun/Database.php';
+        include_once '/opt/lampp/htdocs/ProjetPhp/Commun/nav.php';
+        if (isset($_GET['reference'])) {
+            if (!isset($_GET['choix'])) {
+                supprimer_produit_formulaire();
+            }
+        else {
+                supprimer_produit($_GET['reference']);    
+        }
     }
-?>
-        
-    </body>
+    ?>
+
+</body>
 </html>
