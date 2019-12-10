39533e3c3804ed9ff955b4837b0237d64611dd92
Thomas92330 <49101421+Thomas92330@users.noreply.github.com>
2019-12-10 22:54:05 +0100

    Update Consulter_un_produit.php

--- Manager/Consulter_un_produit.php
+++ Manager/Consulter_un_produit.php
@@ -6,48 +6,17 @@ and open the template in the editor.
 -->
 <html>
     <head>
-        <link rel="stylesheet" href="../Communn/authentification.css">
+        <link rel="stylesheet" href="../Css/Authentification.css">
         <meta charset="UTF-8">
         <title></title>
     </head>
     <body>
-        <?php include '../Commun/nav.php'; 
-              include '../Commun/Database.php'?>
-        <table>
-    <tr>
-       <th>'Référence.'</th>
-       <th>'Libellé.'</th>
-       <th>'Catégorie.'</th>
-       <th>'Marque.'</th>
-       <th>'Quantité.'</th>
-       <th>'Prix.'</th>
-       <th>'Description.'</th>
-    </tr>
-    
-    <?php   
-    $connection = new createConnexion();
-    $connect = $connection->connect();
-    $result = mysqli_query($connect,"SELECT * FROM Produit");
-    while ($res = mysqli_fetch_assoc($result))
-    {
-        if($res['Quantite']==0){
-    echo'<tr>';
-       echo'<th>'.$res['Reference'].'</th>';
-       echo'<th>'.$res['Libelle'].'</th>';
-       echo'<th>'.$res['Categorie'].'</th>';
-       echo'<th>'.$res['Marque'].'</th>';
-       echo'<th>'.$res['Quantite'].'</th>';
-       echo'<th>'.$res['Prix'].'</th>';
-       echo'<th>'.$res['Description'].'</th>';
-       echo'<th>'
-                    . '<form action="Modifier_un_produit.php" method="get">'
-                    . '<input type="radio" id="btn" name="reference" value =' . $res["Reference"] . '/> <label for="btn">Modifier</label>      
-        <input type="submit" value="Valider">'
-                    . '</form>'
-                    . '</th>';
-                    
-    } 
-    }
+        <?php
+        include_once '/opt/lampp/htdocs/ProjetPhp/Commun/Database.php';
+        include_once '/opt/lampp/htdocs/ProjetPhp/Commun/nav.php';
+       
+        consulter_produit();
         ?>
+        
     </body>
 </html>
